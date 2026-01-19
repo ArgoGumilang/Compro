800cb9fe8cefe94dd28c77c94ddd6463a51e6f1a
dnddsf3 <148702929+dnddsf3@users.noreply.github.com>
2026-01-19 18:04:07 +0700

    404

--- src/pages.ts
+++ src/pages.ts
@@ -1,10 +1,28 @@
-// Export all pages
-export { default as ArtikelPage } from './pages/ArtikelPage';
-export { ManajemenBukuPage } from './pages/ManajemenBukuPage';
-export { DataPresensiPage } from './pages/DataPresensiPage';
-export { DataAnggotaPage } from './pages/DataAnggotaPage';
-export { default as PeminjamanAktifPage } from './pages/PeminjamanAktifPage';
-export { default as PengembalianPage } from './pages/PengembalianPage';
-export { default as JatuhTempoPage } from './pages/JatuhTempoPage';
-export { default as LoginPage } from './pages/LoginPage';
-export { default as BarcodePage } from './pages/BarcodePage';
+// ================= PUBLIC =================
+export { default as LoginPage } from "./pages/LoginPage";
+
+// ================= ADMIN =================
+export { default as DashboardPage } from "./pages/DashboardPage";
+export { default as ProfilePage } from "./pages/ProfilePage";
+
+export { DataAnggotaPage } from "./pages/DataAnggotaPage";
+export { ManajemenBukuPage } from "./pages/ManajemenBukuPage";
+export { default as DetailBukuPage } from "./pages/DetailBukuPage";
+
+export { default as PeminjamanAktifPage } from "./pages/PeminjamanAktifPage";
+export { default as PengembalianPage } from "./pages/PengembalianPage";
+export { default as JatuhTempoPage } from "./pages/JatuhTempoPage";
+
+export { DataPresensiPage } from "./pages/DataPresensiPage";
+export { default as BarcodePage } from "./pages/BarcodePage";
+
+// ================= ANGGOTA =================
+export { default as DashAnggota } from "./pages/DashAnggota";
+export { default as DetailBukuAng } from "./pages/DetailBukuAng";
+export { default as PinjamanSaya } from "./pages/PinjamanSaya";
+export { default as Kategori } from "./pages/Kategori";
+export { default as Jelajahi } from "./pages/Jelajahi";
+export { default as ProfileAng } from "./pages/ProfileAng";
+
+// ================= OPTIONAL =================
+export { default as ArtikelPage } from "./pages/ArtikelPage";
